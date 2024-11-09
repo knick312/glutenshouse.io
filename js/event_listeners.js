47d3f1a7e743fba15ef19f5b8a7e9031ae96550a
knick312 <141573999+knick312@users.noreply.github.com>
2024-11-09 12:43:37 -0300

    Update event_listeners.js

--- js/event_listeners.js
+++ js/event_listeners.js
@@ -1,20 +1,32 @@
-// event_listeners.js
-// Event listeners
-document.getElementById("registro-link").addEventListener("click", mostrarRegistro);
-document.getElementById("login-close").addEventListener("click", cerrarFormularios);
-document.getElementById("login-link").addEventListener("click", () => mostrarFormulario("login-overlay"));
 const EventListeners = (function () {
     const registroLink = document.getElementById("registro-link");
     const loginLink = document.getElementById("login-link");
+    const logoutLink = document.getElementById("logout-link");
     const registroClose = document.getElementById("registro-close");
     const loginClose = document.getElementById("login-close");
-    const perfilLink = document.getElementById("perfil-link");
-    const codigoQRButton = document.getElementById("codigo-qr-button");
 
-    registroLink.addEventListener("click", () => Interfaz.mostrarFormulario("registro-overlay"));
-    loginLink.addEventListener("click", () => Interfaz.mostrarFormulario("login-overlay"));
-    registroClose.addEventListener("click", () => Interfaz.ocultarFormulario("registro-overlay"));
-    loginClose.addEventListener("click", () => Interfaz.ocultarFormulario("login-overlay"));
-    perfilLink.addEventListener("click", () => Interfaz.mostrarFormulario("perfil-form"));
-    codigoQRButton.addEventListener("click", Interfaz.mostrarCodigoQR);
+    // Mostrar el formulario de registro
+    registroLink.addEventListener("click", () => {
+        Interfaz.mostrarFormulario("registro-overlay");
+    });
+
+    // Mostrar el formulario de inicio de sesión
+    loginLink.addEventListener("click", () => {
+        Interfaz.mostrarFormulario("login-overlay");
+    });
+
+    // Cerrar el formulario de registro
+    registroClose.addEventListener("click", () => {
+        Interfaz.ocultarFormulario("registro-overlay");
+    });
+
+    // Cerrar el formulario de inicio de sesión
+    loginClose.addEventListener("click", () => {
+        Interfaz.ocultarFormulario("login-overlay");
+    });
+
+    // Cerrar sesión
+    logoutLink.addEventListener("click", () => {
+        Interfaz.cerrarSesion();
+    });
 })();
